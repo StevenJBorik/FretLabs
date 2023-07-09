805f03878552e087e36a102cdffeb568b8011315
Steven Borik <98104308+StevenJBorik@users.noreply.github.com>
2023-07-08 23:53:54 -0500

    upload functionality and api call

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>import UploadButton from '../uploadButton.svelte';</script>
+
+<main>
+    <h1>Welcome to My App</h1>
+    <UploadButton />
+  </main>
+  
